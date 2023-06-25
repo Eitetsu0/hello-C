9c164a57222e2e578bf3fd99ee0cff0ecd4964f1
O-Eitetsu <wtv-cn@126.com>
2023-06-25 20:28:53 +0800

    test codes

--- hello/test.c
+++ hello/test.c
@@ -1,30 +1,62 @@
 #include <stdio.h>
-#define EF 3.5
-#define s(x) EF*x*x
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-int main()
+#define MSG_TRY "try again\n"
+#define MSG_TIMEOUT "timeout\n"
+
+
+int main(int argc, char *argv[])
 {
-	int a=0,b=0,c=0,d=1,e=0,f=0,g=0b1,h=0,i=0,n=0;
-
-	scanf("%d",&a);
-	while(a>0){
-		putchar(a%10+'0');
-		a/=10;
+	char buf[10];
+	int fd, n, i;
+	fd = open("/dev/tty", O_RDONLY|O_NONBLOCK);
+	if(fd<0) {
+		perror("open /dev/tty");
+		exit(1);
 	}
-	for(int i=0;i<10;i++)
-		printf("%c=%d ",'a'+i,*(&a+i));
-	putchar('\n');
+	for(i=0; i<5; i++) {
+		n = read(fd, buf, 10);
+		if(n>=0)
+			break;
+		if(errno!=EAGAIN) {
+			perror("read /dev/tty");
+			exit(1);
+		}
+		sleep(1);
+		write(STDOUT_FILENO, MSG_TRY, strlen(MSG_TRY));
+	}
+	if(i==5)
+		write(STDOUT_FILENO, MSG_TIMEOUT, strlen(MSG_TIMEOUT));
+	else
+		write(STDOUT_FILENO, buf, n);
+	close(fd);
 
-	while((c=getchar())!=EOF)
+	char s[]="root:x::0:root:/root:/bin/bash:";
+	char *token=strtok(s,":");
+	printf("%s\n",token);
+	while((token=strtok(token+strlen(token)+1,":"))!=NULL){
+		printf("%s\n",token);
+	}
+
+	if(argc<2)
+		return EXIT_SUCCESS;
+
+	printf("\nOpening file %s\n\n",argv[1]);
+	FILE *f=fopen(argv[1],"r");
+	if(f==NULL){
+		perror("Open file");
+		fprintf(stderr,"File name: %s , errno=%d\n", argv[1],errno);
+		exit(EXIT_FAILURE);
+	}
+	int c;
+	while((c=fgetc(f))!=EOF){
 		putchar(c);
+	}
+	fclose(f);
 
 	return 0;
 }
-/*
-int s[1000000]={0};
-for(int i=0;i<1000000;i++)
-	s[a[i]]++;
-for(int i=0;i<1000000;i++)
-	if(s[i]>0)
-		printf("%d ",i);
-*/
