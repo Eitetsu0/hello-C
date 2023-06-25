c2067d720f9259375030d29ea12193d44c9d24c9
O-Eitetsu <wtv-cn@126.com>
2023-06-25 20:33:28 +0800

    hello rust

---
+++ rust/guess/src/main.rs
@@ -0,0 +1,44 @@
+use rand::Rng;
+use std::{cmp::Ordering, io};
+
+fn main() {
+    let x = 5;
+    let y = 10;
+
+    println!("x = {x} and y + 2 = {} ,y = {y}", y + 2);
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("Guess the number!");
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a number!");
+                continue;
+            }
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+
+    println!("The secret number is: {}", secret_number);
+}
